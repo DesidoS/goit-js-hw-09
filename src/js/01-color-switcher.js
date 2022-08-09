ba7e02ff7180f2dd2d1a2274522e3e86435ae2a5
Oleksii Kravchenko <djdesidos@gmail.com>
2022-08-09 14:20:16 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,5 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
-const bodyBackground = document.querySelector('body');
 const DELEY = 1000;
 let intervalId = null;
 
@@ -9,17 +8,20 @@ stopBtn.addEventListener("click", stopClick)
 
 function startClick(event) { 
     intervalId = setInterval(changeBody, DELEY);
-    startBtn.disabled = true
-    stopBtn.disabled = false
+    invertBoolean()
 }
 function stopClick(event) { 
     clearInterval(intervalId)
-    startBtn.disabled = false
-    stopBtn.disabled = true
+    invertBoolean()
 }
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 function changeBody() {
-    bodyBackground.style.backgroundColor = getRandomHexColor()
+    document.body.style.backgroundColor = getRandomHexColor()
+}
+
+function invertBoolean() {
+    startBtn.disabled = !startBtn.disabled
+    stopBtn.disabled = !stopBtn.disabled
 }

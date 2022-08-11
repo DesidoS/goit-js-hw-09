8ee599fb4c615961f33cde04aea433183d3bae69
DesidoS <100207277+DesidoS@users.noreply.github.com>
2022-08-11 21:16:19 +0000

    Deploying to gh-pages from @ DesidoS/goit-js-hw-09@edeb328af361366fd3777c0f4dcd9e5a636162b9 🚀

--- 01-color-switcher.99093696.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}function a(){t.disabled=!t.disabled,n.disabled=!n.disabled}t.addEventListener("click",(function(){e=setInterval(o,1e3),a()})),n.addEventListener("click",(function(){clearInterval(e),a()}))}();
-//# sourceMappingURL=01-color-switcher.99093696.js.map

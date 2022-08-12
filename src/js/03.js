14b712d932db10995df599bd86abeea11c7bfec6
Oleksii Kravchenko <djdesidos@gmail.com>
2022-08-12 19:08:31 +0300

    3th task

--- src/js/03.js
+++ src/js/03.js
@@ -2,47 +2,56 @@ import Notiflix from 'notiflix';
 
 const createPromiseForm = document.querySelector('.form');
 const createPromiseBtn = document.querySelector('button');
-
 const data = {};
+
+let intervalId = null;
+data.position = 1;
 
 createPromiseForm.addEventListener('submit', onSubmit);
 createPromiseForm.addEventListener('input', onInput);
 
 function onInput(e) {
   data[e.target.name] = Number.parseInt(e.target.value)
-};
-
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-  setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        // Fulfill
-        resolve({ position, delay });
-      } else {
-        // Reject
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-};
+}
 
 function onSubmit(e) {
-  e.preventDefault();
   createPromiseBtn.disabled = true
+  e.preventDefault();
+  startTimeout()
+}
 
-  let dataDelay = data.delay;
-  let dataStep = data.step;
-  let dataAmount = data.amount;
-  
-  for (let i = 1; i <= dataAmount; i += 1) {
-    createPromise(i, dataDelay)
+function startTimeout() {
+  setTimeout(makePromis, data.delay);
+}
+
+const makePromis = () => {
+  intervalId = setInterval(() => {
+
+    if (data.position === data.amount + 1) {
+      clearInterval(intervalId)
+      createPromiseBtn.disabled = false
+      return;
+    }
+    
+    const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(data)
+      } else {
+        reject(data)
+      }
+    })
+
+    promise
       .then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       })
-    dataDelay += dataStep;
-  }
+      .finally(() => {
+        data.position += 1;
+        data.delay += data.step
+      });
+  }, data.step);
 };
